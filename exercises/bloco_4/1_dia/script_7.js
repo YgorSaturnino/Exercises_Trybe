f8a041e4ef888412b2bc75afd3c9733eec00c0d6
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-04-23 18:54:50 -0300

    Exercicio 7 resolvido com If

--- exercises/bloco_4/1_dia/script_7.js
+++ exercises/bloco_4/1_dia/script_7.js
@@ -1,32 +1,24 @@
-let nota = parseInt("90%");
+let nota = parseInt("%"); //digitar nota aqui
 let resultado;
 
-switch (nota){
-    case (nota >= 90 && nota < 100):
-        resultado = "A";
-        return console.log(resultado);
-        break;
-    case (nota >= 80 && nota < 90):
-        resultado = "B";
-        return console.log(resultado);
-        break;
-    case (nota >= 70 && nota < 80):
-        resultado = "C";
-        return console.log(resultado);
-        break;
-    case (nota >= 60 && nota < 70):
-        resultado = "D";
-        return console.log(resultado);
-        break;
-    case (nota >=50 && nota < 60):
-        resultado = "E";
-        return console.log(resultado);
-        break;
-    case (nota < 50 && nota >= 0):
-        resultado = "F";
-        return console.log(resultado);
-        break;
-    default:
-        return console.log("Número inválido");
-        break;
+if (nota >= 90 && nota <= 100 ){
+    console.log("Nota A")
+}
+else if(nota >= 80 && nota < 90){
+    console.log("Nota B")
+}
+else if(nota >= 70 && nota < 80){
+    console.log("nota C")
+}
+else if(nota >= 60 && nota < 70){
+    console.log("nota D")
+}
+else if(nota >= 50 && nota < 60){
+    console.log("nota E")
+}
+else if(nota >= 0 && nota < 50){
+    console.log("nota F")
+}
+else {
+    console.log("Essa nota não está nos parâmetros")
 }
